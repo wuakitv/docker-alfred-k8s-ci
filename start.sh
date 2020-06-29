22ad5c5c76a680a829522940021a724eccb47840
Javier López-Nieto <javier.lopeznieto8@gmail.com>
2020-06-29 12:54:48 +0200

    Update Dockerfile

--- start.sh
+++ start.sh
@@ -11,6 +11,7 @@ function exit_error {
 [ -n "$PLUGIN_AWS_ACCESS_KEY_ID" ] && export AWS_ACCESS_KEY_ID=$PLUGIN_AWS_ACCESS_KEY_ID
 [ -n "$PLUGIN_AWS_SECRET_ACCESS_KEY" ] && export AWS_SECRET_ACCESS_KEY=$PLUGIN_AWS_SECRET_ACCESS_KEY
 
+# Set the K8S configuration directory as the workspace
 git clone $GIT_URL && cd !$:t
 
 # Validate all YAML configuration via Kustomize, Kubeval and Kube-Score
