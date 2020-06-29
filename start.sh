bf1198297f182b6460a92c9ca7fd963a858cdccb
Javier López-Nieto <javier.lopeznieto8@gmail.com>
2020-06-29 12:39:33 +0200

    Fix image and update script

--- start.sh
+++ start.sh
@@ -11,6 +11,8 @@ function exit_error {
 [ -n "$PLUGIN_AWS_ACCESS_KEY_ID" ] && export AWS_ACCESS_KEY_ID=$PLUGIN_AWS_ACCESS_KEY_ID
 [ -n "$PLUGIN_AWS_SECRET_ACCESS_KEY" ] && export AWS_SECRET_ACCESS_KEY=$PLUGIN_AWS_SECRET_ACCESS_KEY
 
+git clone $GIT_URL && cd !$:t
+
 # Validate all YAML configuration via Kustomize, Kubeval and Kube-Score
 for overlay in overlays/*/; do
   echo -e "\nValidating the build of ${overlay}."
